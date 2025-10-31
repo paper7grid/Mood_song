16c094390c1ce898d5deb6fb149626f275a2eda9
paper7grid <muddassirsadia123@gmail.com>
2025-10-31 16:14:40 -0700

    wrote code to all the songs on data.js

--- data.js
+++ data.js
@@ -7,5 +7,84 @@ const playlists = {
             cover: "covers/happy/sunflower.jpg"
         },
         {
-            name:""
+            name: "One Dance", 
+            artist: "Drake",
+            path: "songs/happy/one_dance.mp3",
+            cover: "covers/happy/one_dance.jpg"
+        },
+        {
+            name: "Dasies",
+            artist: "Justin Bieber",
+            path: "songs folder/Happy/Justin Bieber  DAISIES.mp3",
+            cover: "covers/happy/daisies.jpg"
+        },
+        {
+            name: "Baby",
+            artist: "Justin Bieber",
+            path: "songs folder/Happy/Justin Bieber  Baby ft. Ludacris.mp3",
+            cover: "covers/happy/baby.jpg"
+        },
+        {
+            name: "Luther",
+            artist: "SZA & Kendrick Lamar",
+            path: "songs folder/Happy/Kendrick Lamar - luther (Official Audio).mp3",
+            cover: "covers/happy/luther.jpg"
+
+        }
+
+    ],
+    sad: [
+        {
+            name: "Heather",
+            artist: "Conan Gray",
+            path: "songs folder/sad/@ConanGray - Heather (Lyrics).mp3",
+            cover: "covers/sad/heather.jpeg"
+        },
+        {
+            name: "Prom Dress",
+            artist: "Mxmtoon",
+            path: "songs folder/sad/Mxmtoon - prom dress (Lyrics).mp3",
+            cover: "covers/sad/prom_dress.jpeg"
+        },
+        {
+            name:"Back to Friends"
+            artist: "Sombr",
+            path: "songs folder/sad/sombr - back to friends (official audio).mp3",
+            cover: "covers/sad/back_to_friends.jpeg"
+        },
+        {
+            name: "Nobody Gets Me",
+            artist: "SZA",
+            path: "songs folder/sad/SZA - Nobody Gets Me (Audio).mp3",
+            cover: "covers/sad/nobody.jpeg"
+        }
     ]
+    calm: [
+        {
+            name: "Soft Spot",
+            artist: "Keshi",
+            path: "songs folder/Calm/keshi - Soft Spot (Lyrics).mp3", 
+            covers: "covers/keshi-soft-spot-Cover-Art.jpg"
+        },
+        {
+            name: "Heatstroke",
+            artist: "Khalid",
+            path: "songs folder/Calm/Khalid - Heatstroke (Lyric Video).mp3",
+            cover: "covers/heatstroke.jpeg"
+        },
+        {
+            name: "I Like Me Better",
+            artist: "Lauv",
+            path: "songs folder/Calm/Lauv - I Like Me Better [Official Audio].mp3",
+            cover: "covers/lauv.jpeg"
+        },
+        {
+            name: "The Color Violet",
+            artist: "Tory Lanez",
+            path: "songs folder/Calm/Tory Lanez - The Color Violet (Lyrics).mp3",
+            cover: "covers/the_color.jpg"
+        }
+
+
+    ]
+};
