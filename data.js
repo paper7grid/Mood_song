8504fb5f1dc953333856b1892e388eda4f886027
paper7grid <muddassirsadia123@gmail.com>
2025-10-31 15:31:30 -0700

    added covers + code fro data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const playlists = {
+    happy: [
+        {
+            name: "Sunflower",
+            artist: "Post Malone, Swae Lee",
+            path: "songs/happy/sunflower.mp3"
+            cover: "covers/happy/sunflower.jpg"
+        },
+        {
+            name:""
+    ]
