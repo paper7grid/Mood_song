61b63305c4766703bb5a49598a57c0e94cb07d96
paper7grid <muddassirsadia123@gmail.com>
2025-11-01 00:15:59 -0700

    Updated all the files, and tested them out + cleared synta error, made changes to size

--- data.js
+++ data.js
@@ -3,32 +3,32 @@ const playlists = {
         {
             name: "Sunflower",
             artist: "Post Malone, Swae Lee",
-            path: "songs/happy/sunflower.mp3",
-            cover: "sunflower.jpg"
+            path: "songs folder/happy/sunflower.mp3",
+            cover: "covers/sunflower.jpg"
         },
         {
             name: "One Dance", 
             artist: "Drake",
-            path: "songs/happy/one_dance.mp3",
-            cover: "one_dance.jpg"
+            path: "songs folder/happy/one_dance.mp3",
+            cover: "covers/one_dance.jpg"
         },
         {
             name: "Dasies",
             artist: "Justin Bieber",
             path: "songs folder/Happy/Justin Bieber  DAISIES.mp3",
-            cover: "daisies.jpg"
+            cover: "covers/dasies.jpg"
         },
         {
             name: "Baby",
             artist: "Justin Bieber",
             path: "songs folder/Happy/Justin Bieber  Baby ft. Ludacris.mp3",
-            cover: "baby.jpg"
+            cover: "covers/baby.jpg"
         },
         {
             name: "Luther",
             artist: "SZA & Kendrick Lamar",
             path: "songs folder/Happy/Kendrick Lamar - luther (Official Audio).mp3",
-            cover: "luther.jpg"
+            cover: "covers/luther.jpg"
 
         }
 
@@ -38,25 +38,25 @@ const playlists = {
             name: "Heather",
             artist: "Conan Gray",
             path: "songs folder/sad/@ConanGray - Heather (Lyrics).mp3",
-            cover: "heather.jpeg"
+            cover: "covers/heather.jpeg"
         },
         {
             name: "Prom Dress",
             artist: "Mxmtoon",
             path: "songs folder/sad/Mxmtoon - prom dress (Lyrics).mp3",
-            cover: "prom_dress.jpeg"
+            cover: "covers/prom_dress.jpeg"
         },
         {
             name:"Back to Friends",
             artist: "Sombr",
             path: "songs folder/sad/sombr - back to friends (official audio).mp3",
-            cover: "back_to_friends.jpeg"
+            cover: "covers/back_to_friends.jpeg"
         },
         {
             name: "Nobody Gets Me",
             artist: "SZA",
             path: "songs folder/sad/SZA - Nobody Gets Me (Audio).mp3",
-            cover: ".jpeg"
+            cover: "covers/nobody.jpeg"
         }
     ],
     calm: [
@@ -64,25 +64,25 @@ const playlists = {
             name: "Soft Spot",
             artist: "Keshi",
             path: "songs folder/Calm/keshi - Soft Spot (Lyrics).mp3", 
-            cover: "keshi-soft-spot-Cover-Art.jpg"
+            cover: "covers/keshi-soft-spot-Cover-Art.jpg"
         },
         {
             name: "Heatstroke",
             artist: "Khalid",
             path: "songs folder/Calm/Khalid - Heatstroke (Lyric Video).mp3",
-            cover: "heatstroke.jpeg"
+            cover: "covers/heatstroke.jpeg"
         },
         {
             name: "I Like Me Better",
             artist: "Lauv",
             path: "songs folder/Calm/Lauv - I Like Me Better [Official Audio].mp3",
-            cover: "lauv.jpeg"
+            cover: "covers/lauv.jpeg"
         },
         {
             name: "The Color Violet",
             artist: "Tory Lanez",
             path: "songs folder/Calm/Tory Lanez - The Color Violet (Lyrics).mp3",
-            cover: "the_color.jpg"
+            cover: "covers/the_color.jpg"
         }
 
 
