751b0c6792b41291752e6b625a19ecc70a25795f
paper7grid <muddassirsadia123@gmail.com>
2025-10-31 21:30:18 -0700

    updated more defn.

--- main.js
+++ main.js
@@ -70,3 +70,28 @@ function seekTo() {
 function setVolume() {
   curr_track.volume = volume_slider.value / 100;
 }
+function seekUpdate() {
+  if (!isNaN(curr_track.duration)) {
+    let seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+    
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) currentSeconds = "0" + currentSeconds;
+    if (durationSeconds < 10) durationSeconds = "0" + durationSeconds;
+    if (currentMinutes < 10) currentMinutes = "0" + currentMinutes;
+    if (durationMinutes < 10) durationMinutes = "0" + durationMinutes;
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+function goBack() {
+  window.location.href = "index.html";
+}
+
+let updateTimer;
+loadTrack(track_index);
